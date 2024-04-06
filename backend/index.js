432ffc7a0b21637d8ae7ce49ed8d36bf71e45c6b
Vicki <=>
2024-04-06 16:03:59 -0700

    change to mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,20 @@
-const express = require('express');
+const dotenv = require("dotenv");
+const express = require("express");
+const activitiesRouter = require("./routes/activityRoutes.js");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+
+dotenv.config();
+
 const app = express();
-const cors = require('cors');
-const activitiesRouter = require('./routes/activityRoutes.js'); 
-
-
-
-const port = 3000;
-
-app.use(cors());
 app.use(express.json());
-app.get('/', (req, res) => {
-    res.send('GET request to the homepage')
-})
-app.use("/activity", activitiesRouter)
+app.use("/activities", activitiesRouter);
+app.use(cors());
 
+const MONGODB_URI = 'mongodb://0.0.0.0:27017';
+mongoose.connect(`${MONGODB_URI}`).catch((e) => console.log(e));
 
-app.listen(port, () => {
-  console.log(`Node.js HTTP server is running on port ${port}`);
+app.listen(3000, async () => {
+  console.log("Connected to MongoDB and running at http://localhost:3000");
 });
-
-var MongoClient = require('mongodb').MongoClient;
-async function main(){
-    /**
-     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-     */
-    const url = 'mongodb://0.0.0.0:27017'; // use local host instead of 0.0.0.0 if not working
-    const client = new MongoClient(url);
-    try {
-        await client.connect();
-        console.log("Successfully connected to database!"); 
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
-}
-
-main().catch(console.error);
