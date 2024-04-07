163734c4f931c56ebff541c14a67be07fc98c3dd
Vicki <=>
2024-04-07 10:30:46 -0700

    result pictures

--- frontend/src/components/Home/Results/results.jsx
+++ frontend/src/components/Home/Results/results.jsx
@@ -1,16 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useEffect, useState } from "react";
 import {useLocation} from 'react-router-dom';
-import group1 from "./../assets/Group 7.png";
-import stanley from "./../assets/stanleypark.png";
-import sunset from "./../assets/sunsetbeach.png";
-import falsecreek from "./../assets/falsecreek.png";
+import grind from "./../assets/79grind.png";
+import abby from "./../assets/abbygrind.png";
+import alouette from "./../assets/alouette.png";
 
 import arrow from "./../assets/arrow.png";
 import axios from "axios";
-
-
-
 
 
 const Results = () => {
@@ -21,7 +17,7 @@ const Results = () => {
     const [selected, setSelected] = useState(false);
     const [data, setData] = useState([]);
     const [filteredData, setFilteredData] = useState([]);
-
+ 
     
     const fetchResults = async () => {
         try {
@@ -37,17 +33,17 @@ const Results = () => {
                 console.log(filteredData);
             } 
             if (fields[2] == "intermediate") {
-                let filteredData = data.filter(item => item.difficulty.length <= 3);
+                let filteredData = data.filter(item => item.difficulty.length == 3);
                 setFilteredData(filteredData);
  
                 console.log(filteredData);
             }
-            if (fields[2] == "expert") {
+            if (fields[2] == "expert") { 
                 let filteredData = data.filter(item => item.difficulty.length >= 3);
                 setFilteredData(filteredData);
 
                 console.log(filteredData);
-            }
+            } 
           } catch (error) {
             console.log(error);
           }
@@ -56,16 +52,15 @@ const Results = () => {
 
     useEffect(() => {
         fetchResults();
+      }, [data])
 
-
-      }, [])
     let activities;
       if (filteredData.length >= 3) {
         activities = filteredData.slice(0, 3).map((item, index) => (
             <div key={item.id}>
                 <span className="text-xl">{item.name}</span>
                 <img
-                    src={index === 0 ? stanley : index === 1 ? sunset : falsecreek}
+                    src={index === 0 ? grind : index === 1 ? abby : alouette}
                     alt={item.name}
                     className="h-[300px] w-[300px] object-cover opacity-70 hover:opacity-100 transition duration-300 ease-in-out hover:cursor-pointer"
                     onClick={() => { 
@@ -96,7 +91,7 @@ const Results = () => {
             <span className="text-black font-open_sans text-3xl font-bold">
                 Suggested Activities
             </span>
-
+ 
         </div>
         <div
             className={`${
@@ -105,31 +100,11 @@ const Results = () => {
         >
 
             <div className="flex flex-row h-[60vh] items-center justify-center gap-5 w-screen">
-                {/* <div>
-                    <span className="text-4xl ">name</span>
-                    <img
-                        src={group1}
-                        className="h-3/4 opacity-70 hover:opacity-100 transition duration-300 ease-in-out hover:cursor-pointer"
-                        onClick={() => {
-                            setSelected(true);
-                        }}
-                    />
-                    <div className="group flex flex-row items-center justify-start hover:cursor-pointer hover:opacity-100 opacity-60 transition duration-500 ease-in-out">
-                        <span className="w-[10vw] text-2xl">Select</span>
-                        <div className="w-full flex flex-row justify-end items-end">
-                            <img
-                                src={arrow}
-                                className="h-16 -mb-2 group-hover:opacity-100 opacity-40 transition duration-500 ease-in-out"
-                            />
-                        </div>
-                    </div>
-                </div> */}
-
                 {activities}
 
             </div>
         </div>
-        </div>
+    </div>
     );
 };
 
